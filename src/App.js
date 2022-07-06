769820aa2d544f8767c45939330ed98b9a047f49
dahalh <dahalh@outlook.com>
2022-07-06 11:30:04 +1000

    project initialised

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "react-bootstrap";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      <Button variant="primary">
+        <i className="fa-brands fa-react"></i>react
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
